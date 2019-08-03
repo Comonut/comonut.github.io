685ad3fabe9a4f3941a978714cf007d9d3de8f9c
Comonut <rjuan33@gmail.com>
2019-08-03 15:03:15 +0300

    added analogies

---
+++ analogies/precache-manifest.0a231338ed773328e88dd3d73bf5dd74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3f234d52a9f482b1c60c189fa74ba1",
+    "url": "/analogies/index.html"
+  },
+  {
+    "revision": "b5f520a4b7f93ea2dc77",
+    "url": "/analogies/static/css/main.7c352598.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/analogies/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "b5f520a4b7f93ea2dc77",
+    "url": "/analogies/static/js/main.1da3beef.chunk.js"
+  },
+  {
+    "revision": "037e9099962a43aa1085",
+    "url": "/analogies/static/js/runtime~main.5e5c9093.js"
+  }
+]);
